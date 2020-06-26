1a1065ea99c12e7c3a8d8770c06677a1423c286d
J. Henry <52577797+MrHenry1@users.noreply.github.com>
2020-06-26 09:08:26 -0300

    Add files via upload

---
+++ Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Fields from '../../Components/Form/form.jsx'
+
+function Home() {
+  return (
+    <div id="container">
+       <Fields />
+    </div>
+  )
+}
+
+export default Home;
