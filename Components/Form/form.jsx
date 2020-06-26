6e875d8cecb97b1b09a1f48c05178080c1678d0f
J. Henry <52577797+MrHenry1@users.noreply.github.com>
2020-06-26 09:06:29 -0300

    Add files via upload

--- Components/Form/form.jsx
+++ Components/Form/form.jsx
@@ -1,1 +1,99 @@
+import React, {useState} from 'react'
 
+
+function Fields(props) {
+  
+  const [visible, setVisible] = useState("password")
+  const [count, setCount] = useState(0)
+  const [num, setNumber] = useState(0)
+  const [pais, setPais] = useState("")
+  const [phone, setPhone] = useState(null)
+  const [age, setAge] = useState(null)
+  const [yearcount, setYear] = useState(0)
+  
+  function handlePaisChange(event) {
+    const {value, maxLength} = event.target
+    const textPs = value.slice(0, maxLength)
+    
+    setPais(textPs.toUpperCase())
+    
+    if(value.split("").length <= 2) {
+      setCount(value.length)
+    }
+  }
+  
+  function handlePhoneChange(event) {
+    const { value, maxLength } = event.target
+    const numberPhone = value.slice(0, maxLength)
+    
+    setPhone(numberPhone)
+    
+    if (value.split("").length <= 10) {
+      setNumber(value.length)
+    }
+  }
+  
+  function handleAgeChange(event) {
+    const { value, maxLength } = event.target
+    const numberAge = value.slice(0, maxLength)
+    
+    setAge(numberAge)
+    
+    if (value.split("").length <= 2) {
+      setYear(value.length)
+    }
+  }
+  
+  return (
+    <div id="form">
+      <h1>Form</h1>
+          <form props>
+            <fieldset>
+              <legend>Nome</legend>
+              <span><i class="far fa-user"></i></span>
+              <input type="name" required></input>
+            </fieldset>
+            <fieldset>
+               <legend>Idade</legend>
+               <span><i class="fas fa-baby"></i></span>
+               <input type="number" value={age} maxLength="2" onChange={(e) => handleAgeChange(e)} required></input>
+               <p>{yearcount}/2</p>
+            </fieldset>
+            <fieldset>
+               <legend>Email</legend>
+               <span><i class="fas fa-envelope"></i></span>
+               <input type="email" required></input>
+            </fieldset>
+            <fieldset>
+               <legend>Senha</legend>
+               <span>{visible === 'text' ? <i onClick={() => setVisible('password')} class="fas fa-lock-open"></i> : <i onClick={() => setVisible('text')} class="fas fa-lock"></i>
+               }</span>
+               <input type={visible} required></input>
+               
+            </fieldset>
+            <fieldset>
+               <legend>Telefone</legend>
+               <span><i class="fas fa-phone-square-alt"></i></span>
+               <input type="number" value={phone} maxlength="10" onChange={(e) => handlePhoneChange(e)} required></input>
+               <p>{num}/10</p>
+            </fieldset>
+            <fieldset>
+               <legend>Pais</legend>
+               <span><i class="fas fa-flag"></i></span>
+               <input
+               type="name"
+               maxlength="2"
+               value={pais}
+               onChange={(e) => handlePaisChange(e) }
+                required
+               ></input>
+               <p>{count}/2</p>
+            </fieldset>
+            
+            <button type="submit">Jump!</button>
+          </form>
+        </div>
+  )
+}
+
+export default Fields;
